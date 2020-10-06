af5e81ec784396118fc90dc00f79db7193179bdf
Sebastian villegas <sebasvil20@gmail.com>
2020-10-06 17:13:48 -0500

    Agregando dispatch de tareas

--- src/components/tasks/FormTarea.js
+++ src/components/tasks/FormTarea.js
@@ -11,9 +11,21 @@ const FormTarea = () => {
   //Array destructuring para extraer el proyecto actual
   const [proyectoActual] = proyecto;
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // validar
+
+    //pasar la validacion
+
+    // agregar nueva tarea al state de tareas
+
+    // reiniciar el form
+  };
+
   return (
     <div className="formulario">
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="contenedor-input">
           <input
             type="text"
